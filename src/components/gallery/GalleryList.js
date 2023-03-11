b6857512b83bd38bdca24221d17f8795fe14a68c
VolodymyrIpatenko <81745231+VolodymyrIpatenko@users.noreply.github.com>
2023-03-11 17:34:42 +0200

    prod

--- src/components/gallery/GalleryList.js
+++ src/components/gallery/GalleryList.js
@@ -1,43 +1,40 @@
 const mainGallery = [
   {
     photo: 'https://volodymyripatenko.github.io/victoria-react/rgb/first.jpeg',
-    alt: 'Cooper arch',
+    alt: 'work of student',
   },
   {
     photo: 'https://volodymyripatenko.github.io/victoria-react/rgb/second.jpeg',
-    alt: 'Cooper arch',
+    alt: 'work of student',
   },
   {
     photo: 'https://volodymyripatenko.github.io/victoria-react/rgb/third.jpeg',
-    alt: 'Cooper arch',
+    alt: 'work of student',
   },
-  {
-    photo: 'https://volodymyripatenko.github.io/victoria-react/rgb/fourth.jpeg',
-    alt: 'Macrame with wooden  arch',
-  },
+
   {
     photo: 'https://volodymyripatenko.github.io/victoria-react/rgb/fifth.jpeg',
-    alt: 'Open arms cooper arch',
+    alt: 'work of student',
   },
   {
     photo: 'https://volodymyripatenko.github.io/victoria-react/seventh-new.jpeg',
-    alt: 'Cooper arch',
+    alt: 'work of student',
   },
   {
     photo: 'https://volodymyripatenko.github.io/victoria-react/resized-first.jpeg',
-    alt: 'Cooper arch',
+    alt: 'work of student',
   },
   {
     photo: 'https://volodymyripatenko.github.io/victoria-react/resized-second.jpeg',
-    alt: 'Cooper arch',
+    alt: 'work of student',
   },
   {
     photo: 'https://volodymyripatenko.github.io/victoria-react/resized-third.jpeg',
-    alt: 'Cooper arch',
+    alt: 'work of student',
   },
   {
     photo: 'https://volodymyripatenko.github.io/victoria-react/resized-fourth.jpeg',
-    alt: 'Cooper arch',
+    alt: 'work of student',
   },
 ];
 
